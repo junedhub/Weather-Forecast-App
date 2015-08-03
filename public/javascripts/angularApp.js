858a49db5d5b599ff191d9eda1639ffe6cd6cbfd
justjuned <justjuned@ubuntu.ubuntu-domain>
2015-08-04 00:21:22 +0530

    added comment the code file

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -10,5 +10,6 @@ var weatherApp = angular.module('weatherApp',[
 // Angular configuration goes here
 weatherApp.config(['$urlRouterProvider','$mdThemingProvider', function($urlRouterProvider,$mdThemingProvider){
 	$urlRouterProvider.otherwise('/');
+	//changing the angular material color scheme to light-bule
 	$mdThemingProvider.theme('default').primaryPalette('light-blue');
 }]);
