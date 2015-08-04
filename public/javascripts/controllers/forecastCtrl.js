cb1d0b52abbb237044164e99c481dc036a0de2b2
justjuned <justjuned@ubuntu.ubuntu-domain>
2015-08-04 12:58:45 +0530

    doing some minor refactoring to the code

--- public/javascripts/controllers/forecastCtrl.js
+++ public/javascripts/controllers/forecastCtrl.js
@@ -1,9 +1,9 @@
 angular.module('weatherApp')
-.controller('forecastCtrl',['$scope','weatherAPI', 'cityService',function($scope,weatherAPI,cityService){
+.controller('forecastCtrl',['$scope','weatherAPI','$state', 'cityService',function($scope,weatherAPI,$state,cityService){
 	$scope.weatherForecasts = [];
-		for(var i = 0;  i < cityService.cities.length; i++){
-			$scope.weatherForecasts.push(weatherAPI.get({q: cityService.cities[i], cnt: '14'}));
-		}	
+	$scope.cities = cityService;
+	for(var i = 0;  i < $scope.cities.length; i++)
+		$scope.weatherForecasts.push(weatherAPI.get({q: $scope.cities[i], cnt: '14'}));
 	$scope.convertToCelsius = function(degK){
 		return Math.round(degK - 273.15);
 	};	
